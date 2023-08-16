08b88e6dbc9d78cda0be85b03cc48bcadba5c7bb
Apparky <apparky@protonmail.com>
2023-08-16 12:58:54 +0530

    Apparky

--- MyCRM/settings.py
+++ MyCRM/settings.py
@@ -76,15 +76,14 @@ WSGI_APPLICATION = 'MyCRM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,7 +94,7 @@ DATABASES = {
         'PORT': '3306'
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
